da64fd4d20ad11f8194dedc21458155523f24129
Benjamin Crot <benjamin.crot@gmail.com>
2020-04-01 16:45:51 +0200

    done

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one_attached :photo
+
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+
 end
--- db/migrate/20200401133218_add_column_to_users.rb
+++ db/migrate/20200401133218_add_column_to_users.rb
@@ -2,6 +2,6 @@ class AddColumnToUsers < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
-    add_column :users, :professionnal, :boolean
+    add_column :users, :is_professionnal, :boolean
   end
 end
