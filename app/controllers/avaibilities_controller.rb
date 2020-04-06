c1527d0b45d1c7bcbc199f99b6fcfd3bf2f21219
Tom Levy <tom@solveit.fr>
2020-04-06 15:58:31 +0200

    controlleravaibilities

--- app/controllers/avaibilities_controller.rb
+++ app/controllers/avaibilities_controller.rb
@@ -1,2 +1,30 @@
 class AvaibilitiesController < ApplicationController
+   skip_after_action :verify_policy_scoped, only: :index
+
+  def index
+    @availabilities = current_user.availabilities
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    @availability = Availability.find(params[:id])
+    authorize @availability
+  end
+
+  def update
+    @availability = Availability.find(params[:id])
+    @availability.user = current_user
+    @availability.update(availability_params)
+    redirect_to availabilities_path
+    authorize @availability
+  end
+
+  private
+
+  def availability_params
+    params.require(:availability).permit(:week_day, :breakfast, :lunch, :afterwork)
+  end
+
+end
+
 end
